6eefd74a5f0f0951719ed46a37d50786b91ac2a0
Alexander Cuenca <alex.charco.25@gmail.com>
2022-12-31 15:39:02 -0500

    Arithmetic Operations

--- src/main/java/acc/coding/clases/java/ClasesJava.java
+++ src/main/java/acc/coding/clases/java/ClasesJava.java
@@ -31,7 +31,7 @@ public class ClasesJava {
             - boolean: V/F
             - String(objeto): cadenas de caracteres
         - CUIDADO: no podemos tener 2 variables con el mismo nombre
-        */
+         */
         //    <tipo-dato> <nombre-variable> = <valor> ;
         int var1 = -25;
         int var2 = 10000;
@@ -44,6 +44,38 @@ public class ClasesJava {
         String str1 = "Hola como estas?";
         char c1 = 'a';
         char c2 = 'b';
+
+        //OPERADORES ARIMETICOS
+        // - operaciones matematicas: +,-,*,/,%
+        //SINTAXIS: <valor1> <operacion> <valor2>;
+        int num1 = 10;
+        int num2 = 20;
+        int sum1 = num1 + num2;
+        int sum2 = num2 + num1;
+        int sum3 = 999 + 25;
+        int sum4 = num1 + 10000;
+        int res = sum1 - 100000;
+        int mul = sum2 * 33;
+        int div = 25/2;
+
+        System.out.println("res: "+res);
+        System.out.println("mul: "+mul);
+        System.out.println("div: "+div);
+        
+        System.out.println("sum: " + sum1);
+        System.out.println("sum: " + sum2);
+        System.out.println("sum: " + sum3);
+        System.out.println("sum: " + sum4);
+
+        //REUTILIZACION DE VARIABLES
+        // - declarar una variable una sola vez
+        int var10 = 10;
+        //int var10 = 10;
+        // - reasignar el valor de la variable (variable ya debe existir)
+        // <nombre-variable> = <nuevo-valor>;
+        System.out.println("var10: "+var10);
+        var10 = -999;//cambiar el valor 10 -> -999
+        System.out.println("var10: "+var10);
         
     }
 }
